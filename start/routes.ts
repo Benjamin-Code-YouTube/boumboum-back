79b939ae7f77eb29c3dbd09c9202a364357fbfff
Romain Lanz <romain.lanz@pm.me>
2023-11-19 20:14:58 +0100

    feat: add health route

--- start/routes.ts
+++ start/routes.ts
@@ -20,32 +20,34 @@
 
 import Route from "@ioc:Adonis/Core/Route";
 
+Route.get('health', ({ response }) => response.noContent())
+
 Route.group(() => {
   Route.get("/", async ({ response }) => {
     return response.json("Working.");
   });
-  
+
   // SIGN IN ROUTES
   Route.get("/signin", "UsersController.redirect");
-  
+
   //OAuth CALLBACK
   Route.get("/signin-callback", "UsersController.handleCallback");
-  
+
   Route.post("/logout", "UsersController.logout");
-  
+
   Route.group(() => {
     Route.get("/", "ProfilesController.get");
     Route.post("/", "ProfilesController.store");
   })
   .prefix("profile")
   .middleware("auth:api");
-  
+
   Route.group(() => {
     Route.get("/", "GendersController.index");
   })
   .prefix("genders")
   .middleware("auth:api");
-  
+
   Route.group(() => {
     Route.get("/artists", "SpotifyController.artists");
     Route.get("/tracks", "SpotifyController.tracks");
@@ -53,18 +55,18 @@ Route.group(() => {
   })
   .prefix("spotify")
   .middleware("auth:api");
-  
+
   Route.group(() => {
     /* potential matches */
     Route.get("/", "MatchesController.get");
-    
+
     /* mark match */
     Route.post("/mutual", "MatchesController.mutualMatch");
-    
+
     /* get mutual match history */
     Route.get("/history", "MatchesController.history");
   })
   .prefix("matches")
   .middleware("auth:api");
-  
+
 }).prefix("api");
